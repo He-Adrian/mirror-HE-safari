2673a3861692a4c37c31c4fb54968ec5b1b60e3b
adrian.buzala <adrian.buzala@he-arc.ch>
2021-03-15 03:19:55 -0700

    initial commit

---
+++ Chap04_TodoProject/src/main/java/ch/hearc/spring/todo/JavaEeTodoProject01Application.java
@@ -0,0 +1,13 @@
+package ch.hearc.spring.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeTodoProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEeTodoProject01Application.class, args);
+	}
+
+}
