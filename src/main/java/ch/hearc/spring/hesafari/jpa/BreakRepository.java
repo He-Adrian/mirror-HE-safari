e7ecb608f7f57eb00a8ced34885d75be1ffa5a5f
javaee_2020 <mathias.marty@he-arc.ch>
2021-05-02 09:46:06 -0700

    tset

--- src/main/java/ch/hearc/spring/hesafari/jpa/BreakRepository.java
+++ src/main/java/ch/hearc/spring/hesafari/jpa/BreakRepository.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,7 @@ import ch.hearc.spring.hesafari.model.Break;
 
 public interface BreakRepository extends JpaRepository<Break, Long> {
 // this a dynamic request so we cannot use @query annotation
+	@Autowired
 	public default List<Break> findAllByLocation(String location) {
 		return this.findAll().stream().filter(b -> b.getLocation().toUpperCase().contains(location.toUpperCase()))
 				.collect(Collectors.toList());
