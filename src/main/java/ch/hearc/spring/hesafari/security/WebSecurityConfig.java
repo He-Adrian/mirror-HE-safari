d4c9a62b9f9cb593e95530a6bfbe42a5467d1a64
javaee_2020 <adrian.buzala@he-arc.ch>
2021-05-02 07:05:24 -0700

    removed repo

--- src/main/java/ch/hearc/spring/hesafari/security/WebSecurityConfig.java
+++ src/main/java/ch/hearc/spring/hesafari/security/WebSecurityConfig.java
@@ -1,65 +1,65 @@
-package ch.hearc.spring.hesafari.security;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	private DataSource dataSource;
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		return new CustomUserDetailsService();
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder(11);
-	}
-
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider() {
-		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService());
-		authProvider.setPasswordEncoder(passwordEncoder());
-//		authProvider.setPasswordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
-
-		return authProvider;
-	}
-
-	@Override
-	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authenticationProvider());
-	}
-
-	@Override
-	protected void configure(final HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin().loginPage("/user/login")
-				.usernameParameter("username").defaultSuccessUrl("/").permitAll()
-//          .failureUrl("/login.html?error=true")
-//          .failureHandler(authenticationFailureHandler())
-				.and().logout().logoutUrl("/user/logout").permitAll();
-//          
-//          .deleteCookies("JSESSIONID")
-//          .logoutSuccessUrl("/");
-		// ...
-	}
-}
+//package ch.hearc.spring.hesafari.security;
+//
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.ImportResource;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Autowired
+//	private DataSource dataSource;
+//
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		return new CustomUserDetailsService();
+//	}
+//
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder(11);
+//	}
+//
+//	@Bean
+//	public DaoAuthenticationProvider authenticationProvider() {
+//		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//		authProvider.setUserDetailsService(userDetailsService());
+//		authProvider.setPasswordEncoder(passwordEncoder());
+////		authProvider.setPasswordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+//
+//		return authProvider;
+//	}
+//
+//	@Override
+//	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+//		auth.authenticationProvider(authenticationProvider());
+//	}
+//
+//	@Override
+//	protected void configure(final HttpSecurity http) throws Exception {
+//		http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().formLogin().loginPage("/user/login")
+//				.usernameParameter("username").defaultSuccessUrl("/").permitAll()
+////          .failureUrl("/login.html?error=true")
+////          .failureHandler(authenticationFailureHandler())
+//				.and().logout().logoutUrl("/user/logout").permitAll();
+////          
+////          .deleteCookies("JSESSIONID")
+////          .logoutSuccessUrl("/");
+//		// ...
+//	}
+//}
