f115b8d589f0975d4b6ce792190da640278ab294
javaee_2020 <mathias.marty@he-arc.ch>
2021-05-02 08:46:29 -0700

    remove nxn

--- src/main/java/ch/hearc/spring/hesafari/model/Break.java
+++ src/main/java/ch/hearc/spring/hesafari/model/Break.java
@@ -46,9 +46,9 @@ public class Break {
 
 	@ManyToOne
 	private User owner;
-//
-	@ManyToMany(mappedBy = "attendedBreaks", fetch = FetchType.EAGER)
-	Set<User> attends = Collections.emptySet();
+
+//	@ManyToMany(mappedBy = "attendedBreaks", fetch = FetchType.EAGER)
+//	Set<User> attends = Collections.emptySet();
 
 	public Break() {
 		///
@@ -121,13 +121,13 @@ public class Break {
 		this.description = description;
 	}
 
-	public Set<User> getAttends() {
-		return attends;
-	}
-
-	public void setAttends(Set<User> attends) {
-		this.attends = attends;
-	}
+//	public Set<User> getAttends() {
+//		return attends;
+//	}
+//
+//	public void setAttends(Set<User> attends) {
+//		this.attends = attends;
+//	}
 
 	public User getOwner() {
 		return owner;
