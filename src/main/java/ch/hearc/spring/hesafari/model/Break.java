b1a6d53a4f2010bf4f74d84d1cdb897837c460b8
javaee_2020 <mathias.marty@he-arc.ch>
2021-05-02 11:06:37 -0700

    test

--- src/main/java/ch/hearc/spring/hesafari/model/Break.java
+++ src/main/java/ch/hearc/spring/hesafari/model/Break.java
@@ -29,20 +29,20 @@ public class Break {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long breakID;
-
-	@DateTimeFormat(pattern = "h:mm a")
-	@Column(name = "fromDate", unique = false, nullable = false)
-	private Time from;
-
-	@DateTimeFormat(pattern = "h:mm a")
-	@Column(name = "toDate", unique = false, nullable = false)
-	private Time to;
-
-	@Column(unique = true, nullable = false, length = 20)
-	private String location;
-
-	@Column(unique = true, nullable = false, length = 100)
-	private String description;
+//
+//	@DateTimeFormat(pattern = "h:mm a")
+//	@Column(name = "fromDate", unique = false, nullable = false)
+//	private Time from;
+//
+//	@DateTimeFormat(pattern = "h:mm a")
+//	@Column(name = "toDate", unique = false, nullable = false)
+//	private Time to;
+//
+//	@Column(unique = true, nullable = false, length = 20)
+//	private String location;
+//
+//	@Column(unique = true, nullable = false, length = 100)
+//	private String description;
 
 //	@ManyToOne
 //	private User owner;
@@ -62,13 +62,13 @@ public class Break {
 	 * @param to
 	 * @param location
 	 */
-	public Break(int breakID, Time from, Time to, String location, String description) {
-		this.breakID = breakID;
-		this.from = from;
-		this.to = to;
-		this.location = location;
-		this.description = description;
-	}
+//	public Break(int breakID, Time from, Time to, String location, String description) {
+//		this.breakID = breakID;
+//		this.from = from;
+//		this.to = to;
+//		this.location = location;
+//		this.description = description;
+//	}
 
 	/**
 	 * Getters and Setters
@@ -82,44 +82,37 @@ public class Break {
 		this.breakID = breakID;
 	}
 
-	public Time getFrom() {
-		return from;
-	}
-
-	public void setFrom(String strFrom) {
-//		try {
-//			this.from = new SimpleDateFormat("H:mm").parse(strFrom);
-//		} catch (ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-		this.from = Time.valueOf(String.format("%s:00", strFrom));
-	}
-
-	public Time getTo() {
-		return to;
-	}
-
-	public void setTo(String strTo) {
-//		this.to = Date.valueOf(strFrom);// new SimpleDateFormat("H:mm").parse(strFrom);
-		this.to = Time.valueOf(String.format("%s:00", strTo));
-	}
-
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
+//	public Time getFrom() {
+//		return from;
+//	}
+//
+//	public void setFrom(String strFrom) {
+//		this.from = Time.valueOf(String.format("%s:00", strFrom));
+//	}
+//
+//	public Time getTo() {
+//		return to;
+//	}
+//
+//	public void setTo(String strTo) {
+//		this.to = Time.valueOf(String.format("%s:00", strTo));
+//	}
+//
+//	public String getLocation() {
+//		return location;
+//	}
+//
+//	public void setLocation(String location) {
+//		this.location = location;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
 
 //	public Set<User> getAttends() {
 //		return attends;
